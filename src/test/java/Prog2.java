4e503a8ad8a8e0d93976d85c59dd41c4f17a11f1
sandy0518 <sai.sandeep.906@gmail.com>
2022-09-14 14:39:55 +0530

    another project

---
+++ src/test/java/Prog2.java
@@ -0,0 +1,8 @@
+
+public class Prog2 {
+
+	public static void main(String[] args) {
+	System.out.println("im in testyantra");
+	}
+
+}
---
+++ src/test/java/prog1.java
@@ -0,0 +1,8 @@
+
+public class prog1 {
+
+	public static void main(String[] args) {
+		System.out.println("im in hyd");
+	}
+
+}
